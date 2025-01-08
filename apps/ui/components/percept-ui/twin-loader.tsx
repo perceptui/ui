2eb74079abfa0450a390b0062c5964013a4e9274
Kapil Jangid <kapiljangid040@gmail.com>
2025-01-08 17:06:17 +0530

    feat: enhance TwinLoader component with dynamic className support

--- apps/ui/components/percept-ui/twin-loader.tsx
+++ apps/ui/components/percept-ui/twin-loader.tsx
@@ -82,7 +82,13 @@ type BounceLoaderProps = VariantProps<typeof bounceLoaderVariants> & {
 
 export const TwinLoader = forwardRef<HTMLDivElement, BounceLoaderProps>(
   ({ className, color }) => (
-    <div className="flex-col gap-4 w-full flex items-center justify-center">
+    <div
+      className={cn(
+        className,
+        bounceLoaderVariants({ className, color }),
+        "flex-col gap-4 w-full flex items-center justify-center"
+      )}
+    >
       <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
         <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
       </div>
